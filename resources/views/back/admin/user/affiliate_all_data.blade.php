10ad8fec66f95119f53e80f95d22e7d10983f06a
eabventures-champion <eabventures@gmail.com>
2026-07-14 16:52:48 +0000

    added joined date to affiliate page

--- resources/views/back/admin/user/affiliate_all_data.blade.php
+++ resources/views/back/admin/user/affiliate_all_data.blade.php
@@ -107,7 +107,12 @@
                            @endif
                         </div>
                      </td>
-                     <td> <code style="font-size: 13px; font-weight: bold; color: #7B2828;">{{ $item->referral_code }}</code> </td>
+                     <td> 
+                        <code style="font-size: 13px; font-weight: bold; color: #7B2828; display: block; margin-bottom: 4px;">{{ $item->referral_code }}</code> 
+                        <span class="badge bg-light text-secondary border" style="font-size: 10px; font-weight: 500; text-transform: none; display: inline-block;">
+                           Joined: {{ $item->created_at->format('d M Y') }}
+                        </span>
+                     </td>
                      <td>
                         <a href="javascript:void(0);" onclick="showReferralDetails({{ $item->id }}, '{{ addslashes($item->name) }}')" class="badge bg-info text-dark hover-shadow" style="font-size: 12px; font-weight: 600; text-decoration: none; cursor: pointer; display: inline-block;">
                            {{ $activeCount }}/{{ $totalCount }} referrals
